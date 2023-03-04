1a93a08a0fe1527cac6ce5d6736db0db81fa1ed9
WidsonSilva <silvawidson@gmail.com>
2023-03-04 15:46:49 -0300

    criando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -11,7 +11,45 @@ public class PrimeiroPrograma {
         gato.setCor("Laranja");
         gato.setIdade(1);
 
+        Livro livro1 = new Livro("The Godfather",462);
+
+        System.out.println(livro1+"\n");
         System.out.println(gato);
 
     }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+
 }
